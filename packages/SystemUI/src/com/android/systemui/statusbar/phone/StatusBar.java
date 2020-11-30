58551344249efb301b99fd19c7c313377a4ee125
Lucchetto <lucchetto.tie@live.com>
2020-11-30 22:58:36 +0100

    SystemUI: Animate brightness slider
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -380,6 +380,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     private final VolumeComponent mVolumeComponent;
     private BrightnessMirrorController mBrightnessMirrorController;
+    private BrightnessMirrorController mQuickBrightnessMirrorController;
     private boolean mBrightnessMirrorVisible;
     private BiometricUnlockController mBiometricUnlockController;
     private final LightBarController mLightBarController;
@@ -1335,12 +1336,20 @@ public class StatusBar extends SystemUI implements DemoMode,
                         mBrightnessMirrorVisible = visible;
                         updateScrimController();
                     });
+            mQuickBrightnessMirrorController = new BrightnessMirrorController(
+                    mNotificationShadeWindowView,
+                    mNotificationPanelViewController,
+                    mNotificationShadeDepthControllerLazy.get(),
+                    (visible) -> {
+                        mBrightnessMirrorVisible = visible;
+                        updateScrimController();
+                    });
             fragmentHostManager.addTagListener(QS.TAG, (tag, f) -> {
                 QS qs = (QS) f;
                 if (qs instanceof QSFragment) {
                     mQSBarHeader = ((QSFragment) qs).getHeader();
                     mQSPanel = ((QSFragment) qs).getQsPanel();
-                    mQSPanel.setBrightnessMirror(mBrightnessMirrorController);
+                    mQSPanel.setBrightnessMirror(mQuickBrightnessMirrorController);
                 }
             });
         }
@@ -1632,6 +1641,9 @@ public class StatusBar extends SystemUI implements DemoMode,
         if (mBrightnessMirrorController != null) {
             mBrightnessMirrorController.onDensityOrFontScaleChanged();
         }
+        if (mQuickBrightnessMirrorController != null) {
+            mQuickBrightnessMirrorController.onDensityOrFontScaleChanged();
+        }
         // TODO: Bring these out of StatusBar.
         mUserInfoControllerImpl.onDensityOrFontScaleChanged();
         mUserSwitcherController.onDensityOrFontScaleChanged();
@@ -1658,6 +1670,9 @@ public class StatusBar extends SystemUI implements DemoMode,
         if (mBrightnessMirrorController != null) {
             mBrightnessMirrorController.onOverlayChanged();
         }
+        if (mQuickBrightnessMirrorController != null) {
+            mQuickBrightnessMirrorController.onOverlayChanged();
+        }
         // We need the new R.id.keyguard_indication_area before recreating
         // mKeyguardIndicationController
         mNotificationPanelViewController.onThemeChanged();
@@ -1668,6 +1683,9 @@ public class StatusBar extends SystemUI implements DemoMode,
     public void onUiModeChanged() {
         if (mBrightnessMirrorController != null) {
             mBrightnessMirrorController.onUiModeChanged();
+        }
+        if (mQuickBrightnessMirrorController != null) {
+            mQuickBrightnessMirrorController.onUiModeChanged();
         }
     }
 
@@ -3324,6 +3342,9 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
         if (mBrightnessMirrorController != null) {
             mBrightnessMirrorController.updateResources();
+        }
+        if (mQuickBrightnessMirrorController != null) {
+            mQuickBrightnessMirrorController.updateResources();
         }
 
         mQuickQsTotalHeight = mContext.getResources().getDimensionPixelSize(
